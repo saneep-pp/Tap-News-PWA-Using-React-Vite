bafa7f0c9bf0003c6c080e26655580b7fed40e59
SANEEP P P <61908137+saneep-pp@users.noreply.github.com>
2025-07-07 17:12:40 +0530

    production vite error solved

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/offline.html",
-    "revision": "0.4hn3103gdl"
+    "revision": "0.h7semn1t83o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
